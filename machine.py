91ea3459f51894e79b50f04b7e5670445220df9d
Maaike van Agtmaal <maaike.v8x@hotmail.com>
2017-09-02 17:52:54 +0200

    Afspelen bladmuziek

--- machine.py
+++ machine.py
@@ -9,7 +9,7 @@ MINUUT = 60
 
 class Machine(object):
     def __init__(self):
-        self.motors = list()
+        self.motors = dict()
         self.bladmuziek = Bladmuziek('pachelbel.yml')
         self.tonen = self.bladmuziek.tonen()
         self.bpm = self.bladmuziek.bpm()
@@ -20,19 +20,19 @@ class Machine(object):
     
     def maak_motors(self):
         for toon in self.tonen:
-            self.motors.append(Motor(toon, 2*self.bpm))
+            self.motors[toon] = Motor(toon, 2*self.bpm)
     
     def __str__(self):
         return str(self.motors)
     
     def afspelen(self):
-        for i in range(0, 100):
-            time.sleep(MINUUT / self.bpm)
-            print('BEAT {0}'.format(i))
-            for m in self.motors:
-                m.afspelen()
-
+        for naam, maat in self.maten.items():
+            for noot, tonen in maat.items():
+                time.sleep(MINUUT / self.bpm)
+                for toon in tonen:
+                    self.motors[toon].afspelen()
 
 if __name__ == '__main__':
     machine = Machine()
+    # print(machine.motors)
     machine.afspelen()
