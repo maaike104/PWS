dd61dd525b450c181e66ae6a0b24c4de73e0b1fa
maaike104 <31568251+maaike104@users.noreply.github.com>
2017-10-17 14:06:05 +0200

    Update bestand.py
    Comments toegevoegd

--- bestand.py
+++ bestand.py
@@ -1,4 +1,7 @@
+# Tkinter is een standaard schermpje dat van Python dat je kan aanroepen zodat er een soort keuzemenu tevoorschijn komt, 
+# en die importeer ik hier zodat ik deze kan aanroepen en het bestand wat ik af wil laten spelen kan kiezen.
 from Tkinter import Tk
+# Tkinter gaat voor mij een beschikbaar bestand zoeken en daarvoor heb ik askopenfilename nodig
 from tkFileDialog import askopenfilename
 
 
@@ -8,5 +11,8 @@ class Bestand:
 
     def selecteer_bestand(self):
         Tk().withdraw()
+        """
+        De code kan alleen een yaml bestand lezen, dus hier zeg ik dat het bestandtype .yml of .yaml moet zijn.
+        """
         self.bestandsnaam = askopenfilename(filetypes=[('YAML', ('yaml', 'yml'))],
                                             title='Selecteer een muziekbestand')
