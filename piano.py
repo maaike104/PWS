8930721c34f3b2d8e3076d3302cd35a911ab61b9
maaike104 <31568251+maaike104@users.noreply.github.com>
2017-10-17 14:26:25 +0200

    comments toegevoegd

--- piano.py
+++ piano.py
@@ -1,3 +1,4 @@
+# Hier zorg ik ervoor dat de computer geluid gaat maken als je op run klikt.
 from os import path, getcwd
 from multiprocessing import Process
 
@@ -8,9 +9,15 @@ class Piano(object):
     def __init__(self, tonenmap):
         self.tonenmap = tonenmap
 
+    """
+    Hier start ik een proces zodat de computer twee tonen tegelijk kan afspelen.
+    """
     def afspelen_op_achtergrond(self, toon):
         p = Process(target=self.afspelen, args=(toon,))
         p.start()
-
+    
+    """
+    Hier leest de computer de audiobestanden en speelt ze af.
+    """
     def afspelen(self, toon):
         playsound(path.join(getcwd(), self.tonenmap, '{0}.wav'.format(toon)))
